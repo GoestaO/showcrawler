e051dd2b2387a4a3458b520486c803ec1670adac
Gösta Ostendorf <gostendorf@zalando.de>
2018-02-10 21:41:14 +0100

    Make file locations a bit more generic so that the scripts can be run in a cronjob

--- rmz.py
+++ rmz.py
@@ -36,28 +36,30 @@ if __name__ == '__main__':
     download_folder = config.get('downloadfolder')
     quality = config.get('quality')
     shows = config.get('shows')
-
     # Iterate through the entries and fetch the title and link, which is the relevant data
     for entry in d['entries']:
+        raw_title = entry['title']
+        link = entry['link']
+
+        # Fetch show infos from the guessit library
+        show_info = get_show_information(raw_title)
+        title = show_info['title']
         for show in shows:
-            raw_title = entry['title']
-            link = entry['link']
-
-            # Fetch show infos from the guessit library
-            show_info = get_show_information(raw_title)
-
+            season = ''
+            episode = ''
             # Check, if show_info contains the keys 'title', 'episode' and 'screen_size' to avoid KeyErrors
-            if 'title' in show_info and 'season' in show_info and 'episode' in show_info and 'screen_size' in show_info:
-                title = show_info['title']
-                season = show_info['season']
-                episode = show_info['episode']
+            if 'title' in show_info and 'screen_size' in show_info:
+                if 'season' in show_info and 'episode' in show_info:
+                    season = show_info['season']
+                    episode = show_info['episode']
                 screen_size = show_info['screen_size']
-
                 if show == title and quality == screen_size and not download_exists(title=title, season=season,
                                                                                     episode=episode):
+
                     # create crawljob and upload to server
                     create_crawljob_and_upload(jobname=show, link=link, download_folder=download_folder)
 
-                    print("create crawljob for " + title)
-                    # save download to avoid multiple downloads of the same file
+                    #save download to avoid multiple downloads of the same file
                     persist_download(title=title, season=season, episode=episode)
+
+
