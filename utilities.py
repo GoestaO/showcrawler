db7ba196bbc97df1d2892d884e8bcb043684ce9c
Gösta <goesta.o@gmail.com>
2018-06-30 13:37:59 +0200

    FTP login more flexible and robust against failed attempts

--- utilities.py
+++ utilities.py
@@ -1,11 +1,19 @@
 from pathlib import Path
 import yaml
 import pysftp
-from ftplib import FTP
+from ftplib import FTP, error_reply
 import os
 from guessit import guessit
 from bs4 import BeautifulSoup
 import requests
+
+
+def read_config(path_to_file: str):
+    with open(path_to_file, 'r') as stream:
+        try:
+            return yaml.load(stream)
+        except yaml.YAMLError as exc:
+            return None
 
 
 CURRENT_FOLDER = os.path.dirname(os.path.realpath(__file__))
@@ -13,11 +21,31 @@ WATCH_FOLDER = CURRENT_FOLDER + "/folderwatch"
 CONFIG_FILE = CURRENT_FOLDER + "/config.yml"
 FTP_CONFIG = CURRENT_FOLDER + "/ftp.yml"
 DB_FILENAME = CURRENT_FOLDER + '/downloads.db'
+connection = read_config(FTP_CONFIG).get('ftp_connection')
+username = connection.get('username')
+passwd = connection.get('password')
+port = connection.get('port')
+folder = connection.get('folderwatch')
+host = connection.get('host')
+port = connection.get('port')
 
 
 def generate_absolute_path_mediaserver(folder):
     rootfolder = "/share/Web/temp/"
     return "{}{}".format(rootfolder, folder)
+
+
+def get_ftp_connection(host, port, username, password):
+    ftp = FTP()
+    try:
+        ftp.connect(host=host, port=port)
+        ftp.login(user=username, passwd=password)
+        ftp.cwd(folder)
+    except error_reply:
+        print(error_reply)
+    finally:
+        ftp.quit()
+    return ftp
 
 
 def generate_download_folder(folder):
@@ -37,37 +65,19 @@ def create_crawljob_and_upload(jobname: str, link: str, download_folder):
     push_file_to_ftp(f)
 
 
-def read_config(path_to_file: str):
-    with open(path_to_file, 'r') as stream:
-        try:
-            return yaml.load(stream)
-        except yaml.YAMLError as exc:
-            return None
-
-
 def push_file_to_ftp(file):
     connection = read_config(FTP_CONFIG).get('ftp_connection')
     username = connection.get('username')
     f = open(file.name, 'rb')
     filename = os.path.basename(file.name)
-    ftp = FTP(user=username, passwd=connection.get('password'))
-    ftp.connect(host=connection.get('host'), port=connection.get('port'))
-    ftp.cwd(connection.get('folderwatch'))
-    ftp.storbinary('STOR ' + filename, f)
-    f.close()
-    ftp.quit()
-
-
-def push_files_to_ftp(folder: str, filelist: list):
-    connection = read_config(FTP_CONFIG)
-    srv = pysftp.Connection(host = connection.get('host'), username=connection.get('username'), password=connection.get('password'))
-    with srv.cd(folder):  # chdir to public
-        for file in filelist:
-            srv.put(file)  # upload file to nodejs/
-
-    # Closes the connection
-    srv.close()
-
+    ftp = get_ftp_connection(host=host, port=port, username=username, password=passwd)
+    try:
+        ftp.storbinary('STOR ' + filename, f)
+        f.close()
+    except error_reply:
+        print(error_reply)
+    finally:
+        ftp.quit()
 
 def log_download(name: str):
     with open("history.txt", "a") as f:
@@ -87,4 +97,3 @@ def beautiful_soup(raw_url):
 
 if __name__ == "__main__":
     print(CONFIG_FILE)
-
